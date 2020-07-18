3e445f90805eb33017fb12b2ba01029a638e3cbb
Ryan King <r.r.king@student.unsw.edu.au>
2020-07-18 18:18:54 +1000

    wrote backend and basic frontend

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var letterRequests = [];
+// request = {letterId, userIds[] initials, subject, body, replies[]}
+// reply = {letterId, userId, initials, body}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.emit("refreshLists", letterRequests);
+
+  socket.on("sendLetter", (userId, letterId, initials, body) => {
+    console.log("sent letter:" + body);
+    letterRequests.push({userIds: [userId], letterId: letterId, initials: initials, body: body, replies:[]});
+    io.emit("refreshLists", letterRequests);
+  });
+
+  socket.on("replyLetter", (userId, requestId, replyBody, initials) => {
+    console.log("reply to letter:" + requestId);
+    let request = letterRequests.find((element) => element.letterId == requestId);
+    let replyId = '_' + Math.random().toString(36).substr(2, 9);
+    request.userIds.push(userId);
+    request.replies.push({letterId: replyId, initials: initials, body: replyBody});
+    io.emit("refreshLists", letterRequests);
+  });
 });
 
-http.listen(8000, () => {
-  console.log('listening on *:8000');
-});
+http.listen(process.env.PORT || 8000);
